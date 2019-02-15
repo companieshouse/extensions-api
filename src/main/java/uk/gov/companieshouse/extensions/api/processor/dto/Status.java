a0788ea85c02f662f975da4eae18c5e51b537526
Matthew Culpin <mculpin@companieshouse.gov.uk>
2019-02-15 13:22:19 +0000

    missed some tabs

--- src/main/java/uk/gov/companieshouse/extensions/api/processor/dto/Status.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/processor/dto/Status.java
@@ -6,5 +6,5 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Status {
-	StatusEnum status = StatusEnum.OPEN;
+    StatusEnum status = StatusEnum.OPEN;
 }
--- src/main/java/uk/gov/companieshouse/extensions/api/processor/dto/StatusEnum.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/processor/dto/StatusEnum.java
@@ -2,10 +2,10 @@ package uk.gov.companieshouse.extensions.api.processor.dto;
 
 public enum StatusEnum {
     OPEN("Open");
-	
-	private String description;
-	
-	private StatusEnum(String desc) {
-	     this.description = desc;
-	}
+
+    private String description;
+
+    private StatusEnum(String desc) {
+      this.description = desc;
+    }
 }
