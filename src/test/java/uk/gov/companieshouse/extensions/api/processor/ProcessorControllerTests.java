8d6423902aab3e5705f761f59607aafdfb2dcc65
Matthew Culpin <mculpin@companieshouse.gov.uk>
2019-02-20 09:07:13 +0000

    Processor test and input service test stub

---
+++ src/test/java/uk/gov/companieshouse/extensions/api/processor/ProcessorControllerTests.java
@@ -0,0 +1,39 @@
+package uk.gov.companieshouse.extensions.api.processor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import uk.gov.companieshouse.extensions.api.processor.controller.ProcessorController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = ProcessorController.class)
+public class ProcessorControllerTests {
+ 
+    public static String URL = "/api/extensions/processor/a1/status";
+
+    @Autowired
+    private MockMvc mockMvc;
+ 
+    
+    @Test
+    public void testCreateExtensionRequestResource() throws Exception {
+      RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
+             URL)
+              .contentType(MediaType.APPLICATION_JSON)
+              .accept(MediaType.APPLICATION_JSON);
+
+      MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+      assertEquals("OPEN", result.getResponse().getContentAsString());
+    }
+
+}
