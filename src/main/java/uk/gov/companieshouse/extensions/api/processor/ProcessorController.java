236a404c50720c28aa518588bdc0c8562785b3ac
markpit <mpitten@companieshouse.gov.uk>
2019-03-27 17:20:04 +0000

    Adding a global base endpoint

--- src/main/java/uk/gov/companieshouse/extensions/api/processor/ProcessorController.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/processor/ProcessorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/extensions/processor")
+@RequestMapping("${api.endpoint.extensions}")
 public class ProcessorController {
 
     @PostMapping("/{requestId}/status")
--- src/main/java/uk/gov/companieshouse/extensions/api/requests/RequestsController.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/requests/RequestsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/company/{companyNumber}/extensions/requests")
+@RequestMapping("${api.endpoint.extensions}")
 public class RequestsController {
 
     @Autowired
