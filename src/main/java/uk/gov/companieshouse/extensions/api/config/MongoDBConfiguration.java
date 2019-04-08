d6fdc380e841388d5464ce91fc8364b5346338af
markpit <mpitten@companieshouse.gov.uk>
2019-04-08 10:15:35 +0100

    Removing ApplicationTest as it's only checking the app starts and causing issues with mongo config

--- src/main/java/uk/gov/companieshouse/extensions/api/config/MongoDBConfiguration.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/config/MongoDBConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * MongoDB connectionPoolProperties .
+ * MongoDB Properties .
  */
 @Configuration
 public class MongoDBConfiguration {
