856e3aadbb24b181b5923e4fb6c834448121fc44
markpit <mpitten@companieshouse.gov.uk>
2019-04-08 17:08:12 +0100

    De-lombok-ing

--- src/main/java/uk/gov/companieshouse/extensions/api/requests/ExtensionRequest.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/requests/ExtensionRequest.java
@@ -7,14 +7,10 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import uk.gov.companieshouse.extensions.api.reasons.ExtensionReason;
 import uk.gov.companieshouse.service.links.Links;
 
-@Getter
-@Setter
 @Document(collection = "extension_requests")
 public class ExtensionRequest {
 
@@ -37,6 +33,69 @@ public class ExtensionRequest {
 
     private List<ExtensionReason> reasons;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+
+    public LocalDateTime getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(LocalDateTime requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public RequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RequestStatus status) {
+        this.status = status;
+    }
+
+    public LocalDate getAccountingPeriodStartDate() {
+        return accountingPeriodStartDate;
+    }
+
+    public void setAccountingPeriodStartDate(LocalDate accountingPeriodStartDate) {
+        this.accountingPeriodStartDate = accountingPeriodStartDate;
+    }
+
+    public LocalDate getAccountingPeriodEndDate() {
+        return accountingPeriodEndDate;
+    }
+
+    public void setAccountingPeriodEndDate(LocalDate accountingPeriodEndDate) {
+        this.accountingPeriodEndDate = accountingPeriodEndDate;
+    }
+
+    public List<ExtensionReason> getReasons() {
+        return reasons;
+    }
+
+    public void setReasons(List<ExtensionReason> reasons) {
+        this.reasons = reasons;
+    }
 
     public String toString() {
       return "User " + user + " Acc period start: " + accountingPeriodStartDate + "  Acc period end: " + accountingPeriodEndDate;
