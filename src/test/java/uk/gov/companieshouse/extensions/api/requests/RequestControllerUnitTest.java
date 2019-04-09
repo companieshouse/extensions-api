3f96d7b225dd436158e44f3ae25dfadae2426b59
John Terry <jterry@companieshouse.gov.uk>
2019-04-09 11:09:21 +0100

    fix unit test from latest merge

--- src/test/java/uk/gov/companieshouse/extensions/api/requests/RequestControllerUnitTest.java
+++ src/test/java/uk/gov/companieshouse/extensions/api/requests/RequestControllerUnitTest.java
@@ -6,13 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
-import uk.gov.companieshouse.extensions.api.reasons.ReasonsControllerUnitTest;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -30,9 +29,9 @@ public class RequestControllerUnitTest {
 
     @Test
     public void createsExtensionRequestResource() {
-        ResponseEntity<String> response = controller.createExtensionRequestResource(dummyRequest());
-        assertEquals("Request received: User Micky Mock Acc period start: 2018-12-12  Acc period " +
-            "end: 2018-12-12", response.getBody());
+        ResponseEntity<ExtensionRequest> response =
+            controller.createExtensionRequestResource(dummyRequest());
+        assertEquals("Micky Mock", response.getBody().getUser());
     }
 
     @Test
@@ -55,15 +54,15 @@ public class RequestControllerUnitTest {
 
     @Test
     public void canDeleteExtensionRequest() {
-
+        boolean response = controller.deleteExtensionRequestById("123");
+        assertFalse(response);
     }
 
-    public ExtensionRequest dummyRequest() {
-        ExtensionRequest request = new ExtensionRequest();
+    public ExtensionCreateRequest dummyRequest() {
+        ExtensionCreateRequest request = new ExtensionCreateRequest();
         request.setUser("Micky Mock");
         request.setAccountingPeriodEndDate(LocalDate.of(2018, 12, 12));
         request.setAccountingPeriodStartDate(LocalDate.of(2018, 12, 12));
-        request.setReasons(Arrays.asList(ReasonsControllerUnitTest.dummyReason()));
         return request;
     }
 
