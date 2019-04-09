7ae207f5b67b24b869b30c9343a87983f35e3264
markpit <mpitten@companieshouse.gov.uk>
2019-04-09 13:48:18 +0100

    Removin unused import

--- src/main/java/uk/gov/companieshouse/extensions/api/requests/ExtensionRequest.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/requests/ExtensionRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
