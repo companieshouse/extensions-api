a8689552815a27900a1dbdb715cb876560b19f14
John Terry <jterry@companieshouse.gov.uk>
2019-04-24 14:14:34 +0100

    handled Exceptions in controller plus test

--- src/main/java/uk/gov/companieshouse/extensions/api/attachments/AttachmentsController.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/attachments/AttachmentsController.java
@@ -2,18 +2,14 @@ package uk.gov.companieshouse.extensions.api.attachments;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import uk.gov.companieshouse.extensions.api.requests.CreatedBy;
 import uk.gov.companieshouse.extensions.api.requests.ERICHeaderParser;
+import uk.gov.companieshouse.service.ServiceException;
 import uk.gov.companieshouse.service.ServiceResult;
+import uk.gov.companieshouse.service.rest.err.Err;
+import uk.gov.companieshouse.service.rest.err.Errors;
 import uk.gov.companieshouse.service.rest.response.ChResponseBody;
 import uk.gov.companieshouse.service.rest.response.PluggableResponseEntityFactory;
 
@@ -39,16 +35,24 @@ public class AttachmentsController {
     @PostMapping("/{requestId}/reasons/{reasonId}/attachments")
     public ResponseEntity<ChResponseBody<AttachmentDTO>> uploadAttachmentToRequest(
             @RequestParam("file") MultipartFile file, @PathVariable String requestId,
-            @PathVariable String reasonId, HttpServletRequest servletRequest) throws Exception {
+            @PathVariable String reasonId, HttpServletRequest servletRequest) {
         CreatedBy createdBy = new CreatedBy();
         createdBy.setId(ericHeaderParser.getUserId(servletRequest));
         createdBy.setEmail(ericHeaderParser.getEmail(servletRequest));
         createdBy.setForename(ericHeaderParser.getForename(servletRequest));
         createdBy.setSurname(ericHeaderParser.getSurname(servletRequest));
 
-        ServiceResult<AttachmentDTO> result = attachmentsService.addAttachment(file,
-            servletRequest.getRequestURI(), requestId, reasonId);
-        return responseEntityFactory.createResponse(result);
+        try {
+            ServiceResult<AttachmentDTO> result = attachmentsService.addAttachment(file,
+                servletRequest.getRequestURI(), requestId, reasonId);
+            return responseEntityFactory.createResponse(result);
+        } catch(ServiceException e) {
+            Errors errors = new Errors();
+            errors.addError(Err.serviceErrBuilder()
+                .withError(e.getMessage())
+                .build());
+            return responseEntityFactory.createResponse(ServiceResult.invalid(errors));
+        }
     }
 
     @DeleteMapping("/{requestId}/reasons/{reasonId}/attachments/{attachmentId}")
---
+++ src/test/java/uk/gov/companieshouse/extensions/api/attachments/AttachmentsControllerUnitTest.java
@@ -0,0 +1,52 @@
+package uk.gov.companieshouse.extensions.api.attachments;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+import uk.gov.companieshouse.extensions.api.Utils.Utils;
+import uk.gov.companieshouse.extensions.api.requests.ERICHeaderParser;
+import uk.gov.companieshouse.service.ServiceException;
+import uk.gov.companieshouse.service.rest.response.PluggableResponseEntityFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AttachmentsControllerUnitTest {
+
+    @Mock
+    private AttachmentsService attachmentsService;
+
+    @Mock
+    private ERICHeaderParser ericHeaderParser;
+
+    @Mock
+    private HttpServletRequest servletRequest;
+
+    @Test
+    public void willReturnErrorsJson() throws Exception {
+        when(servletRequest.getRequestURI()).thenReturn("url");
+        when(attachmentsService.addAttachment(any(MultipartFile.class), anyString(), anyString(),
+            anyString())).thenThrow(new ServiceException("exception error"));
+
+        AttachmentsController controller =
+            new AttachmentsController(PluggableResponseEntityFactory.buildWithStandardFactories(),
+                attachmentsService, ericHeaderParser);
+
+        ResponseEntity entity = controller.uploadAttachmentToRequest(Utils.mockMultipartFile(),
+            "123","1234", servletRequest);
+
+        assertEquals("{\"errors\":[{\"error\":\"exception error\",\"type\":\"ch:service\"}]}",
+            new ObjectMapper().writeValueAsString(entity.getBody()));
+        assertEquals(HttpStatus.BAD_REQUEST, entity.getStatusCode());
+    }
+}
