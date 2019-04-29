3f31862cf2851320bc27540c043bfff6c4501e7e
John Terry <jterry@companieshouse.gov.uk>
2019-04-29 10:08:43 +0100

    address comments

--- README.md
+++ README.md
@@ -4,5 +4,14 @@
 ## Integration Tests
 The integration tests currently run off of the Waldorf environment.
 Dummy data is inserted into this database from the concourse pipeline.
-The extension_requests collection is cleared down as the first task and a single
-extension request is inserted. This contains the basic data of a full request to assert against.
+The first concourse integration-test task will clear down the extension_requests collection and
+several dummy documents will be inserted.
+
+If you are running integration tests locally then you must ensure that your chs-mongo db contains
+the relevant dummy data.
+Run the following command from within the project root folder
+```
+mongoimport --jsonArray --host chs-mongo:27017 --db extension_requests --collection
+extension_requests --file src/test/resources/mongoTestData.json
+```
+This will import the relevant dummy data to your local mongo instance.
--- src/test/java/uk/gov/companieshouse/extensions/api/requests/RequestRepositoryIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/extensions/api/requests/RequestRepositoryIntegrationTest.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.companieshouse.extensions.api.Utils.Utils;
 import uk.gov.companieshouse.extensions.api.attachments.Attachment;
 import uk.gov.companieshouse.extensions.api.reasons.ExtensionReasonEntity;
-import uk.gov.companieshouse.extensions.api.reasons.ExtensionReasonEntityBuilder;
 import uk.gov.companieshouse.service.links.Links;
 
 import java.time.LocalDate;
@@ -49,15 +49,16 @@ public class RequestRepositoryIntegrationTest {
 
     @Test
     public void canSaveRequestToDB() {
-        ExtensionRequestFullEntity expectedEntity = dummyRequest();
+        ExtensionRequestFullEntity expectedEntity = Utils.dummyRequestEntity();
+        expectedEntity.setId(UUID.randomUUID().toString());
 
         ExtensionRequestFullEntity savedEntity = requestsRepository.insert(expectedEntity);
 
         assertNotNull(savedEntity.getId());
         assertTrue(savedEntity.getReasons().isEmpty());
-        assertEquals(LocalDate.of(2018, 2, 1), savedEntity.getAccountingPeriodEndOn());
-        assertEquals(LocalDate.of(2018, 1, 1), savedEntity.getAccountingPeriodStartOn());
-        assertEquals(null, savedEntity.getEtag());
+        assertEquals(LocalDate.of(2019, 12, 12), savedEntity.getAccountingPeriodEndOn());
+        assertEquals(LocalDate.of(2018, 12, 12), savedEntity.getAccountingPeriodStartOn());
+        assertEquals("etag", savedEntity.getEtag());
         assertEquals(Status.OPEN, savedEntity.getStatus());
         assertEquals(expectedEntity.getCreatedBy(), savedEntity.getCreatedBy());
     }
@@ -68,8 +69,7 @@ public class RequestRepositoryIntegrationTest {
             requestsRepository.findById(REQUEST_2)
                 .orElseThrow(() -> new Exception("Request not found in DB"));
 
-        String reasonId = UUID.randomUUID().toString();
-        ExtensionReasonEntity expectedReason = dummyReason(reasonId);
+        ExtensionReasonEntity expectedReason = Utils.dummyReasonEntity();
         expectedEntity.setReasons(Arrays.asList(expectedReason));
         ExtensionRequestFullEntity savedEntity = requestsRepository.save(expectedEntity);
 
@@ -79,8 +79,8 @@ public class RequestRepositoryIntegrationTest {
             .findAny()
             .orElseThrow(() -> new Exception("Reason expected in request object"));
 
-        assertEquals("text", actualReason.getAdditionalText());
-        assertEquals(reasonId, actualReason.getId());
+        assertEquals("string", actualReason.getAdditionalText());
+        assertNotNull(actualReason.getId());
         assertEquals("illness", actualReason.getReason());
     }
 
@@ -119,35 +119,5 @@ public class RequestRepositoryIntegrationTest {
         assertEquals(expectedAttachment.getContentType(), attachment.getContentType());
         assertEquals(expectedAttachment.getSize(), attachment.getSize());
         assertEquals(links, attachment.getLinks());
-    }
-
-    private ExtensionRequestFullEntity dummyRequest() {
-        CreatedBy createdBy = new CreatedBy();
-        createdBy.setEmail("emailAddress");
-        createdBy.setForename("Joe");
-        createdBy.setSurname("Bloggs");
-        createdBy.setId("123");
-
-        return ExtensionRequestFullEntityBuilder
-            .newInstance()
-            .withAccountingPeriodEndOn(LocalDate.of(2018, 2, 1))
-            .withAccountingPeriodStartOn(LocalDate.of(2018, 1, 1))
-            .withCreatedBy(createdBy)
-            .withCreatedOn(() -> LocalDateTime.of(2019, 1, 1, 1, 1))
-            .withStatus()
-            .build();
-    }
-
-    private ExtensionReasonEntity dummyReason(String reasonId) {
-        ExtensionReasonEntity reason = ExtensionReasonEntityBuilder
-            .getInstance()
-            .withAdditionalText("text")
-            .withStartOn(LocalDate.of(2019, 1, 1))
-            .withEndOn(LocalDate.of(2019, 2, 1))
-            .withReason("illness")
-            .withLinks("url")
-            .build();
-        reason.setId(reasonId);
-        return reason;
     }
 }
