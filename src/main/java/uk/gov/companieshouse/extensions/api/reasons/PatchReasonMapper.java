6faca19a307f4b0436b503efe55ceb651c6fd90f
John Terry <jterry@companieshouse.gov.uk>
2019-05-05 12:54:08 +0100

    sort imports

--- src/main/java/uk/gov/companieshouse/extensions/api/reasons/PatchReasonMapper.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/reasons/PatchReasonMapper.java
@@ -1,6 +1,9 @@
 package uk.gov.companieshouse.extensions.api.reasons;
 
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
