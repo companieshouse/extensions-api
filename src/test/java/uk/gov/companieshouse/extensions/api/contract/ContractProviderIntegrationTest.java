982b914a872ccd8335dd1ef5345a741bbeb27419
John Terry <jterry@companieshouse.gov.uk>
2019-07-01 14:01:11 +0100

    remove unneeded deps

--- src/test/java/uk/gov/companieshouse/extensions/api/contract/ContractProviderIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/extensions/api/contract/ContractProviderIntegrationTest.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.extensions.api.contract;
 
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import au.com.dius.pact.provider.junit.Provider;
@@ -13,9 +12,6 @@ import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
 import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
 import uk.gov.companieshouse.extensions.api.groups.ContractProvider;
-import uk.gov.companieshouse.extensions.api.requests.ExtensionRequestFullEntity;
-import uk.gov.companieshouse.extensions.api.requests.ExtensionRequestsRepository;
-import uk.gov.companieshouse.extensions.api.requests.Status;
 
 @Category(ContractProvider.class)
 @RunWith(SpringRestPactRunner.class)
@@ -26,9 +22,6 @@ public class ContractProviderIntegrationTest {
 
     @TestTarget
     public final Target target = new SpringBootHttpTarget();
-
-    @Autowired
-    private ExtensionRequestsRepository repository;
 
     @State("I have a valid OPEN request for 00006400 with requestId aaaaaaaaaaaaaaaaaaaaaaa4")
     public void toPatchState() {}
