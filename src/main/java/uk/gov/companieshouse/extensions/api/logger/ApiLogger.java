6ae9714cd2fef83453af2304682351e537609b90
markpit <mpitten@companieshouse.gov.uk>
2019-07-08 17:25:23 +0100

    Changing the ApiLogger to put default data into a Map

--- src/main/java/uk/gov/companieshouse/extensions/api/logger/ApiLogger.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/logger/ApiLogger.java
@@ -5,6 +5,7 @@ import uk.gov.companieshouse.extensions.api.Application;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -20,43 +21,51 @@ public class ApiLogger {
         COMPANY_NUMBER.remove();
     }
 
-    private String prefix(String message) {
-        return "CompanyNumber = " + COMPANY_NUMBER.get() + " - Thread id " + Thread.currentThread().getId() + " - " + message;
+    /**
+     * Populates the default data that needs to be logged
+     */
+    private Map<String, Object> getDefaultDataMap() {
+        Map<String, Object> logData = new HashMap<>();
+        logData.put("company_number", COMPANY_NUMBER.get());
+        logData.put("thread_id", Thread.currentThread().getId());
+        return logData;
+    }
+
+    private Map<String, Object> getDataMap(Map<String, Object> values) {
+        Map<String, Object> defaultValues = getDefaultDataMap();
+        defaultValues.putAll(values);
+        return defaultValues;
     }
 
     public void debug(String message) {
-        LOG.debug(prefix(message));
+        LOG.debug(message, getDefaultDataMap());
     }
 
     public void debug(String message, Map<String, Object> values) {
-        LOG.debug(prefix(message), values);
+        LOG.debug(message, getDataMap(values));
     }
 
     public void info(String message) {
-        LOG.info(prefix(message));
-    }
-
-    public void info(String companyNumber, String message) {
-        LOG.info(prefix(companyNumber + " - " + message));
+        LOG.info(message, getDefaultDataMap());
     }
 
     public void info(String message, Map<String, Object> values) {
-        LOG.info(prefix(message), values);
+        LOG.info(message, getDataMap(values));
     }
 
     public void error(Exception e) {
-        LOG.error(prefix(" "), e);
+        LOG.error(e.getMessage(), e, getDefaultDataMap());
     }
 
     public void error(String message) {
-        LOG.error(prefix(message));
+        LOG.error(message, getDefaultDataMap());
     }
 
     public void error(String message, Exception e) {
-        LOG.error(prefix(message), e);
+        LOG.error(message, e, getDefaultDataMap());
     }
 
     public void error(String message, Map<String, Object> values) {
-        LOG.error(prefix(message), values);
+        LOG.error(message, getDataMap(values));
     }
 }
