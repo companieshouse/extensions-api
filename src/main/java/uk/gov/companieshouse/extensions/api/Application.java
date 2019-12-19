bbc410ac14390e71f8339a2212796bc72e8a45ad
Matthew Culpin <mculpin@companieshouse.gov.uk>
2019-12-19 11:21:37 +0000

    Easier solution to mongo DST problem

--- src/main/java/uk/gov/companieshouse/extensions/api/Application.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/Application.java
@@ -3,6 +3,9 @@ package uk.gov.companieshouse.extensions.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
@@ -11,4 +14,10 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
