3950eb408746eece1c25497a487b177a7c1105ac
markpit <mpitten@companieshouse.gov.uk>
2020-03-30 11:55:28 +0100

    Adding 2 new status for processing, PROCESSING and CHIPS_SENT

--- src/main/java/uk/gov/companieshouse/extensions/api/requests/Status.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/requests/Status.java
@@ -2,5 +2,7 @@ package uk.gov.companieshouse.extensions.api.requests;
 
 public enum Status {
     OPEN,
+    PROCESSING,
+    CHIPS_SENT,
     SUBMITTED
 }
--- src/test/java/uk/gov/companieshouse/extensions/api/requests/PatchRequestMapperUnitTest.java
+++ src/test/java/uk/gov/companieshouse/extensions/api/requests/PatchRequestMapperUnitTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.time.LocalDate;
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -16,22 +17,30 @@ import uk.gov.companieshouse.service.links.Links;
 @Category(Unit.class)
 public class PatchRequestMapperUnitTest {
 
-    @Test
-    public void canMergeAPatchEntityIntoADatabaseEntity() {
-        RequestStatus patchEntity = new RequestStatus();
-        patchEntity.setStatus(Status.SUBMITTED);
-        patchEntity.setIsAutoAccepted(true);
+    private ExtensionRequestFullEntity dbEntity;
+    private Links links;
 
-        ExtensionRequestFullEntity dbEntity = new ExtensionRequestFullEntity();
+    @Before
+    public void setup() {
+        dbEntity = new ExtensionRequestFullEntity();
         dbEntity.setId("12345");
         dbEntity.setAccountingPeriodEndOn(LocalDate.of(2018,1,1));
-        Links links = new Links();
+
+        links = new Links();
         links.setLink(ExtensionsLinkKeys.SELF, "something");
         dbEntity.setLinks(links);
 
         ExtensionReasonEntity reason = new ExtensionReasonEntity();
         reason.setId("reason1");
         dbEntity.setReasons(Arrays.asList(reason));
+    }
+
+    @Test
+    public void canMergeAPatchEntityIntoADatabaseEntity_Submitted() {
+        RequestStatus patchEntity = new RequestStatus();
+        patchEntity.setStatus(Status.SUBMITTED);
+        patchEntity.setIsAutoAccepted(true);
+
         ExtensionRequestFullEntity mappedEntity = PatchRequestMapper.INSTANCE
             .patchEntity(patchEntity, dbEntity);
 
@@ -42,5 +51,38 @@ public class PatchRequestMapperUnitTest {
         assertEquals("reason1", dbEntity.getReasons().get(0).getId());
         assertTrue(dbEntity.getIsAutoAccepted());
     }
-  
+
+    @Test
+    public void canMergeAPatchEntityIntoADatabaseEntity_Processing() {
+        RequestStatus patchEntity = new RequestStatus();
+        patchEntity.setStatus(Status.PROCESSING);
+        patchEntity.setIsAutoAccepted(true);
+
+        ExtensionRequestFullEntity mappedEntity = PatchRequestMapper.INSTANCE
+            .patchEntity(patchEntity, dbEntity);
+
+        assertEquals("12345", mappedEntity.getId());
+        assertEquals(LocalDate.of(2018,1,1), mappedEntity.getAccountingPeriodEndOn());
+        assertEquals(links, mappedEntity.getLinks());
+        assertEquals(Status.PROCESSING, dbEntity.getStatus());
+        assertEquals("reason1", dbEntity.getReasons().get(0).getId());
+        assertTrue(dbEntity.getIsAutoAccepted());
+    }
+
+    @Test
+    public void canMergeAPatchEntityIntoADatabaseEntity_Chips_Sent() {
+        RequestStatus patchEntity = new RequestStatus();
+        patchEntity.setStatus(Status.CHIPS_SENT);
+        patchEntity.setIsAutoAccepted(true);
+
+        ExtensionRequestFullEntity mappedEntity = PatchRequestMapper.INSTANCE
+            .patchEntity(patchEntity, dbEntity);
+
+        assertEquals("12345", mappedEntity.getId());
+        assertEquals(LocalDate.of(2018,1,1), mappedEntity.getAccountingPeriodEndOn());
+        assertEquals(links, mappedEntity.getLinks());
+        assertEquals(Status.CHIPS_SENT, dbEntity.getStatus());
+        assertEquals("reason1", dbEntity.getReasons().get(0).getId());
+        assertTrue(dbEntity.getIsAutoAccepted());
+    }
 }
