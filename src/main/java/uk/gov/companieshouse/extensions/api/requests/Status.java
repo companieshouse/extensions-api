91bd52e09b98c5abe5b19c2875cb02366fba6ea7
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-08-04 15:34:10 +0100

    LFA-1926 Added the new rejected status when max length would be exceeded

--- src/main/java/uk/gov/companieshouse/extensions/api/requests/Status.java
+++ src/main/java/uk/gov/companieshouse/extensions/api/requests/Status.java
@@ -1,6 +1,19 @@
 package uk.gov.companieshouse.extensions.api.requests;
 
 public enum Status {
+    /**
+     * Status of a newly created extension request whose details are being populated.
+     */
     OPEN,
-    SUBMITTED
+
+    /**
+     * An extension request that has been submitted for processing.
+     */
+    SUBMITTED,
+
+    /**
+     * Indicates that an extension request was automatically rejected as the accounts
+     * filing date has already been extended to the maximum period possible.
+     */
+    REJECTED_MAX_EXT_LENGTH_EXCEEDED
 }
